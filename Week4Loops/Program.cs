abfb29d88f7fc6442ff0e7218e7b288ec37a38c8
Jekaterina Seli <jekaterina.selistseva@tktk.ee>
2023-03-03 14:05:36 +0200

    add week4

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿//FOR-LOOP
+//DRY-principe - Do Not Repeat Yourself
+
+int i = 0;
+
+for (i = 0; i < 5; i++) //i++ = i + i
+{
+    Console.WriteLine("We don't need no education");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
+
+//Console.Writeline("We don't need no education.");
+//Console.Writeline("We don't need no education.");
+//Console.Writeline("We don't need no education.");
+//Console.Writeline("We don't need no education.");
